3550284f68232f49bda35d4737469727d9937549
SlyMarbo <the.sly.marbo@googlemail.com>
2026-07-07 19:50:05 +0100

    vdm: Added copyright headers
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

--- internal/vdm/vdm.go
+++ internal/vdm/vdm.go
@@ -1,3 +1,8 @@
+// Copyright 2026 The Firefly Authors.
+//
+// Use of this source code is governed by a BSD 3-clause
+// license that can be found in the LICENSE file.
+
 // Package vdm provides functionality for recording a dependency set.
 package vdm
 
