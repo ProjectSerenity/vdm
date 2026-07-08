84a6ecb748c17ec7ea253fae3bd2ff9130705f68
SlyMarbo <the.sly.marbo@googlemail.com>
2026-07-08 22:24:01 +0100

    digest: Copied the filesystem digesting code into a new package
    This implementation has the same API as vendeps.DigestFiles and
    vendeps.DigestDirectory, but the implementation uses iterators,
    rather than slices of filenames/digests. This is more efficient
    for large numbers of files and is much more elegant to test. As
    a result, this version is much better-tested.
    
    In due course, this will replace the code in vendeps, but for now
    it's just a duplicate.
    
    Signed-off-by: SlyMarbo <the.sly.marbo@googlemail.com>

---
+++ internal/digest/digest.go
@@ -0,0 +1,189 @@
+// Copyright 2026 The Firefly Authors.
+//
+// Use of this source code is governed by a BSD 3-clause
+// license that can be found in the LICENSE file.
+
+package digest
+
+import (
+	"crypto"
+	_ "crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"io"
+	"io/fs"
+	"iter"
+	"path"
+	"strings"
+)
+
+const (
+	digestHash = crypto.SHA256
+	digestName = "sha256"
+)
+
+// digestFile takes a filesystem, an optional path
+// prefix, and an iterator of filenames and returns
+// another iterator, which will yield either a digest
+// for the previous filename or an error.
+func digestFile(fsys fs.FS, prefix string, filenames iter.Seq2[string, error]) iter.Seq2[string, error] {
+	hashBuf := make([]byte, digestHash.Size())
+	return func(yield func(string, error) bool) {
+		for filename, err := range filenames {
+			if err != nil {
+				if !yield("", err) {
+					return
+				}
+
+				continue
+			}
+
+			if strings.Contains(filename, "\n") {
+				if !yield("", fmt.Errorf("filenames with newlines are not allowed: found %q", prefix+filename)) {
+					return
+				}
+
+				continue
+			}
+
+			f, err := fsys.Open(prefix + filename)
+			if err != nil {
+				if !yield("", fmt.Errorf("failed to open %s%s: %v", prefix, filename, err)) {
+					return
+				}
+
+				continue
+			}
+
+			fh := digestHash.New()
+			_, err = io.Copy(fh, f)
+			if err != nil {
+				if !yield("", fmt.Errorf("failed to read %s%s: %v", prefix, filename, err)) {
+					return
+				}
+
+				continue
+			}
+
+			if err = f.Close(); err != nil {
+				if !yield("", fmt.Errorf("failed to close %s%s: %v", prefix, filename, err)) {
+					return
+				}
+
+				continue
+			}
+
+			if !yield(fmt.Sprintf("%x  %s\n", fh.Sum(hashBuf[:0]), filename), nil) {
+				return
+			}
+		}
+	}
+}
+
+// iterateFilenames is a simple iterator that returns
+// each filename in sequence wiht a nil errors.
+func iterateFilenames(filenames []string) iter.Seq2[string, error] {
+	return func(yield func(string, error) bool) {
+		for _, filename := range filenames {
+			if !yield(filename, nil) {
+				return
+			}
+		}
+	}
+}
+
+// Files produces the digest for a set of named
+// files and their contents in a filesystem. This is
+// performed by hashing one line of text for each file.
+// Each line consists of the hexadecimal digest of the
+// file's contents, two spaces (\x20), the relative
+// filename, and a newline (\x0a).
+//
+// Filenames containing a newline (\x0a) are not allowed.
+// The slice of filenames will be sorted to ensure consistent
+// behaviour.
+//
+// The final digest is formatted as the hash algorithm
+// name, a colon (\x3a), and the hexadecimal digest.
+func Files(fsys fs.FS, filenames []string) (string, error) {
+	h := digestHash.New()
+	for line, err := range digestFile(fsys, "", iterateFilenames(filenames)) {
+		if err != nil {
+			return "", err
+		}
+
+		io.WriteString(h, line)
+	}
+
+	result := digestName + ":" + base64.StdEncoding.EncodeToString(h.Sum(nil))
+
+	return result, nil
+}
+
+// iterateDir walks a directory, emitting the filenames
+// below the given directory, ignoring any individual
+// filenames specified. The prefix, if any, is trimmed
+// from each filename.
+func iterateDir(fsys fs.FS, dir, prefix string, ignore ...string) iter.Seq2[string, error] {
+	// Make a lookup for the ignored files to reduce
+	// the complexity.
+	ignored := make(map[string]bool, len(ignore))
+	for _, name := range ignore {
+		ignored[name] = true
+	}
+
+	return func(yield func(string, error) bool) {
+		// As we yield all errors, fs.WalkDir will
+		// never return an error itself.
+		fs.WalkDir(fsys, dir, func(name string, d fs.DirEntry, err error) error {
+			if err != nil {
+				if !yield("", err) {
+					return fs.SkipAll
+				}
+			}
+
+			if ignored[name] {
+				return nil
+			}
+
+			if d != nil && !d.IsDir() {
+				if !yield(strings.TrimPrefix(name, prefix), nil) {
+					return fs.SkipAll
+				}
+			}
+
+			return nil
+		})
+	}
+}
+
+// Directory produces the digest for a directory
+// and its contents in a filesystem. This is performed
+// by hashing one line of text for each file, with the
+// files sorted into lexographical order. Each line
+// consists of the hexadecimal digest of the file's
+// contents, two spaces (\x20), the relative filename,
+// and a newline (\x0a).
+//
+// Filenames containing a newline (\x0a) are not allowed.
+//
+// Any filenames listed in ignore are not included in
+// the hashing process.
+//
+// The final digest is formatted as the hash algorithm
+// name, a colon (\x3a), and the hexadecimal digest.
+func Directory(fsys fs.FS, dir string, ignore ...string) (string, error) {
+	h := digestHash.New()
+	prefix := strings.TrimSuffix(dir, path.Base(dir))
+	for line, err := range digestFile(fsys, prefix, iterateDir(fsys, dir, prefix, ignore...)) {
+		if err != nil {
+			return "", err
+		}
+
+		io.WriteString(h, line)
+	}
+
+	result := digestName + ":" + base64.StdEncoding.EncodeToString(h.Sum(nil))
+
+	return result, nil
+}
---
+++ internal/digest/digest_test.go
@@ -0,0 +1,422 @@
+// Copyright 2026 The Firefly Authors.
+//
+// Use of this source code is governed by a BSD 3-clause
+// license that can be found in the LICENSE file.
+
+package digest
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"iter"
+	"path/filepath"
+	"testing"
+	"testing/fstest"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"golang.org/x/tools/txtar"
+)
+
+func TestDigestFile(t *testing.T) {
+	tests := []struct {
+		Name    string
+		FS      fs.FS
+		Prefix  string
+		Entries []testEntry
+		Want    []testEntry
+	}{
+		{
+			Name: "straightforward",
+			FS:   txtarFS(t, "subdirectories"),
+			Entries: []testEntry{
+				{E: errors.New("input error")},
+				{S: "nonexistant.txt"},
+				{S: "foo/bar/a/b/c.txt"},
+				{S: "foo/bar/baz.txt"},
+			},
+			Want: []testEntry{
+				{E: errors.New("input error")},
+				{E: errors.New("failed to open nonexistant.txt: open nonexistant.txt: file does not exist")},
+				{S: "0c47cda934d53d7ca29d822a59531dcf6d36cbd9740a4fd0b867a0343910a715  foo/bar/a/b/c.txt\n"},
+				{S: "181210f8f9c779c26da1d9b2075bde0127302ee0e3fca38c9a83f5b1dd8e5d3b  foo/bar/baz.txt\n"},
+			},
+		},
+		{
+			Name: "weird",
+			FS: fstest.MapFS{
+				"newline\nfile.txt": {
+					Data:    []byte{1, 2, 3},
+					Mode:    0o644,
+					ModTime: time.Now(),
+				},
+			},
+			Entries: []testEntry{
+				{S: "newline\nfile.txt"},
+			},
+			Want: []testEntry{
+				{E: errors.New(`filenames with newlines are not allowed: found "newline\nfile.txt"`)},
+			},
+		},
+		{
+			Name: "errors",
+			FS:   badFS{},
+			Entries: []testEntry{
+				{S: "open"},
+				{S: "read"},
+				{S: "close"},
+			},
+			Want: []testEntry{
+				{E: errors.New("failed to open open: bad open")},
+				{E: errors.New("failed to read read: bad read")},
+				{E: errors.New("failed to close close: bad close")},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			// Do the entries as a group.
+			got := collect(digestFile(test.FS, test.Prefix, iterate(test.Entries)))
+			if diff := cmp.Diff(test.Want, got, compareErrors); diff != "" {
+				t.Fatalf("digestFile(): mismatch (-want, +got)\n%s", diff)
+			}
+
+			// Do the entries individually.
+			for i, entry := range test.Entries {
+				got := collectSingle(digestFile(test.FS, test.Prefix, iterateSingle(entry)))
+				if diff := cmp.Diff(test.Want[i], got, compareErrors); diff != "" {
+					t.Fatalf("digestFile(%d): mismatch (-want, +got)\n%s", i, diff)
+				}
+			}
+		})
+	}
+}
+
+func TestFiles(t *testing.T) {
+	tests := []struct {
+		Name  string
+		FS    fs.FS
+		Files []string
+		Want  string
+		Error string
+	}{
+		{
+			Name:  "nonexistant",
+			FS:    txtarFS(t, "small"),
+			Files: []string{"nonexistant.txt"},
+			Error: "failed to open nonexistant.txt: open nonexistant.txt: file does not exist",
+		},
+		{
+			Name:  "small",
+			FS:    txtarFS(t, "small"),
+			Files: []string{"foo/bar.txt"},
+			Want:  "sha256:nXbUeK5RNiJs1oQQWJa3D5t0HpyVd6lqNMUcYaIS5hQ=",
+		},
+		{
+			Name: "subdirectories",
+			FS:   txtarFS(t, "subdirectories"),
+			Files: []string{
+				"foo/bar/a/b/c.txt",
+				"foo/bar/baz.txt",
+			},
+			Want: "sha256:XjdtpK2cfWjxTP9RvL5f7/YJaeAncW+MQB1SrUD9JIY=",
+		},
+		{
+			Name: "unsorted",
+			FS:   txtarFS(t, "subdirectories"),
+			Files: []string{
+				"foo/bar/baz.txt",
+				"foo/bar/a/b/c.txt",
+			},
+			Want: "sha256:EMDAMuiLe5TMiYMznf26bfMsvyWyEgzSeKcVCkHfLDY=", // The order matters, so the digest is different from above.
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			final, err := Files(test.FS, test.Files)
+			if test.Error != "" {
+				if err == nil {
+					t.Fatalf("Files(): unexpected lack of error")
+				}
+
+				e := err.Error()
+				if e != test.Error {
+					t.Fatalf("Files(): got wrong error:\nGot:  %s\nWant: %s", e, test.Error)
+				}
+
+				// All good.
+				return
+			}
+
+			if err != nil {
+				t.Fatalf("Files(): %v", err)
+			}
+
+			if final != test.Want {
+				t.Fatalf("Files(): digest mismatch:\nGot:  %s\nWant: %s", final, test.Want)
+			}
+		})
+	}
+}
+
+func TestIterateDir(t *testing.T) {
+	tests := []struct {
+		Name   string
+		FS     fs.FS
+		Dir    string
+		Prefix string
+		Ignore []string
+		Want   []testEntry
+	}{
+		{
+			Name: "small",
+			FS:   txtarFS(t, "small"),
+			Dir:  "foo",
+			Want: []testEntry{
+				{S: "foo/bar.txt"},
+			},
+		},
+		{
+			Name:   "subdirectories",
+			FS:     txtarFS(t, "subdirectories"),
+			Dir:    "foo/bar",
+			Prefix: "foo/",
+			Ignore: []string{
+				"foo/bar/ignored.txt",
+			},
+			Want: []testEntry{
+				{S: "bar/a/b/c.txt"},
+				{S: "bar/baz.txt"},
+			},
+		},
+		{
+			Name: "errors",
+			FS:   badFS{},
+			Dir:  ".",
+			Ignore: []string{
+				"read",
+				"close",
+			},
+			Want: []testEntry{
+				{E: errors.New("file does not exist")},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			// Do the entries as a group.
+			got := collect(iterateDir(test.FS, test.Dir, test.Prefix, test.Ignore...))
+			if diff := cmp.Diff(test.Want, got, compareErrors); diff != "" {
+				t.Fatalf("iterateDir(): mismatch (-want, +got)\n%s", diff)
+			}
+
+			// Do the first individually.
+			first := collectSingle(iterateDir(test.FS, test.Dir, test.Prefix, test.Ignore...))
+			if diff := cmp.Diff(test.Want[0], first, compareErrors); diff != "" {
+				t.Fatalf("iterateDir(first): mismatch (-want, +got)\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestDirectory(t *testing.T) {
+	tests := []struct {
+		Name   string
+		FS     fs.FS
+		Dir    string
+		Ignore []string
+		Want   string
+		Error  string
+	}{
+		{
+			Name:  "nonexistant",
+			FS:    txtarFS(t, "small"),
+			Dir:   "bar",
+			Error: "open bar: file does not exist",
+		},
+		{
+			Name: "small",
+			FS:   txtarFS(t, "small"),
+			Dir:  "foo",
+			Want: "sha256:nXbUeK5RNiJs1oQQWJa3D5t0HpyVd6lqNMUcYaIS5hQ=",
+		},
+		{
+			Name: "subdirectories",
+			FS:   txtarFS(t, "subdirectories"),
+			Dir:  "foo/bar",
+			Ignore: []string{
+				"foo/bar/ignored.txt",
+			},
+			Want: "sha256:69LmaQn9kFxSV2OfnxzyvV4l8iKCB9EnSVuDkjTnn9I=",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			got, err := Directory(test.FS, test.Dir, test.Ignore...)
+			if test.Error != "" {
+				if err == nil {
+					t.Fatalf("Directory(): unexpected lack of error")
+				}
+
+				e := err.Error()
+				if e != test.Error {
+					t.Fatalf("Directory(): got wrong error:\nGot:  %s\nWant: %s", e, test.Error)
+				}
+
+				// All good.
+				return
+			}
+
+			if err != nil {
+				t.Fatalf("Directory(): %v", err)
+			}
+
+			if got != test.Want {
+				t.Fatalf("Directory(): digest mismatch:\nGot:  %s\nWant: %s", got, test.Want)
+			}
+		})
+	}
+}
+
+func txtarFS(t *testing.T, name string) fs.FS {
+	t.Helper()
+	ar, err := txtar.ParseFile(filepath.Join("testdata", name+".txtar"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fsys, err := txtar.FS(ar)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return fsys
+}
+
+// The error types we're using have unexported
+// fields. Annoyingly, the least clumsy fix is
+// to write a custom comparer that just looks
+// at the error message.
+var compareErrors = cmp.Comparer(func(a, b error) bool {
+	if a == nil && b == nil {
+		return true
+	}
+
+	if a == nil || b == nil {
+		return false
+	}
+
+	return a.Error() == b.Error()
+})
+
+type testEntry struct {
+	S string
+	E error
+}
+
+func iterate(entries []testEntry) iter.Seq2[string, error] {
+	return func(yield func(string, error) bool) {
+		for _, entry := range entries {
+			if !yield(entry.S, entry.E) {
+				return
+			}
+		}
+	}
+}
+
+func iterateSingle(entry testEntry) iter.Seq2[string, error] {
+	return func(yield func(string, error) bool) {
+		yield(entry.S, entry.E)
+	}
+}
+
+func collect(entries iter.Seq2[string, error]) []testEntry {
+	var out []testEntry
+	for s, e := range entries {
+		out = append(out, testEntry{S: s, E: e})
+	}
+
+	return out
+}
+
+func collectSingle(entries iter.Seq2[string, error]) testEntry {
+	for s, e := range entries {
+		return testEntry{S: s, E: e}
+	}
+
+	panic("got no values from iterator")
+}
+
+type badFS struct{}
+
+var (
+	_ fs.FS        = badFS{}
+	_ fs.ReadDirFS = badFS{}
+)
+
+func (badFS) Open(name string) (fs.File, error) {
+	switch name {
+	case "open":
+		return nil, fmt.Errorf("bad open")
+	case "read":
+		return badReadFile{}, nil
+	case "close":
+		return badCloseFile{}, nil
+	default:
+		return nil, fs.ErrNotExist
+	}
+}
+
+func (badFS) ReadDir(name string) ([]fs.DirEntry, error) {
+	if name != "." {
+		return nil, fs.ErrNotExist
+	}
+
+	entries := []fs.DirEntry{
+		simpleDirEntry{"open", false, 0o644},
+		simpleDirEntry{"read", false, 0o644},
+		simpleDirEntry{"close", false, 0o644},
+	}
+
+	return entries, nil
+}
+
+type simpleDirEntry struct {
+	name  string
+	isDir bool
+	mode  fs.FileMode
+}
+
+var _ fs.DirEntry = simpleDirEntry{}
+
+func (e simpleDirEntry) Name() string               { return e.name }
+func (e simpleDirEntry) IsDir() bool                { return e.isDir }
+func (e simpleDirEntry) Type() fs.FileMode          { return e.mode }
+func (e simpleDirEntry) Info() (fs.FileInfo, error) { return nil, fs.ErrInvalid }
+
+type badReadFile struct{}
+
+var _ fs.File = badReadFile{}
+
+func (badReadFile) Stat() (fs.FileInfo, error) { return nil, nil }
+func (badReadFile) Close() error               { return nil }
+func (badReadFile) Read(b []byte) (int, error) {
+	return 0, errors.New("bad read")
+}
+
+type badCloseFile struct{}
+
+var _ fs.File = badCloseFile{}
+
+func (badCloseFile) Stat() (fs.FileInfo, error) { return nil, nil }
+func (badCloseFile) Close() error               { return errors.New("bad close") }
+func (badCloseFile) Read(b []byte) (int, error) {
+	n := copy(b, "foo")
+	return n, io.EOF
+}
